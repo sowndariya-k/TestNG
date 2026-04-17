4b09f0259bea810ead90ffee6edf0c79181655d7
sowndariya <sowndariyadeveloper@gmail.com>
2026-04-17 07:12:41 +0530

    update in code

--- src/test/java/com/TestNG/Selenium/NewTest.java
+++ src/test/java/com/TestNG/Selenium/NewTest.java
@@ -7,8 +7,11 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 
 public class NewTest {
@@ -26,7 +29,13 @@ public class NewTest {
   }
   @Test
   public void validation() {
-	  driver.findElement(By.id("login2")).click();
+	  WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+	  WebElement loginBtn = wait.until(
+	      ExpectedConditions.elementToBeClickable(By.id("login2"))
+	  );
+
+	  loginBtn.click();
 	  driver.findElement(By.id("loginusername")).sendKeys("sowndariya");
 	  driver.findElement(By.id("loginpassword")).sendKeys("Sow@911!");
       driver.findElement(By.xpath("//button[text()='Log in']")).click();
