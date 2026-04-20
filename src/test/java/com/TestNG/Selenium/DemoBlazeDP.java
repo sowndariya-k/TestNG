d70260bf9af231ef16b3541ea2f0be2fccd4b0d1
sowndariya <sowndariyadeveloper@gmail.com>
2026-04-20 16:23:09 +0530

    updated

--- src/test/java/com/TestNG/Selenium/DemoBlazeDP.java
+++ src/test/java/com/TestNG/Selenium/DemoBlazeDP.java
@@ -5,99 +5,99 @@ import java.time.Duration;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.*;
 import org.testng.Assert;
 import org.testng.annotations.*;
+
 public class DemoBlazeDP {
 
-	    WebDriver driver;
-	    
-	    @DataProvider(name = "validData")
-	    public Object[][] validData() {
-	        return new Object[][] {
-	            {"sowndariya", "Sow@911!"}
-	        };
-	    }
+    WebDriver driver;
 
-	    @DataProvider(name = "invalidData")
-	    public Object[][] invalidData() {
-	        return new Object[][] {
-	            {"*sk", "Sow@911!", "User does not exist."},
-	            {"sowndariya", "sow@911", "Wrong password."}
-	        };
-	    }
+    @DataProvider(name = "validData")
+    public Object[][] validData() {
+        return new Object[][] {
+            {"sowndariya", "Sow@911!"}
+        };
+    }
 
-	    @BeforeMethod
-	    @Parameters("browser")
-	    public void setup(String browser) {
+    @DataProvider(name = "invalidData")
+    public Object[][] invalidData() {
+        return new Object[][] {
+            {"*sk", "Sow@911!", "User does not exist."},
+            {"sowndariya", "sow@911", "Wrong password."}
+        };
+    }
 
-	        if (browser.equalsIgnoreCase("chrome")) {
-	            driver = new ChromeDriver(new ChromeOptions());
-	        } else if (browser.equalsIgnoreCase("firefox")) {
-	            driver = new FirefoxDriver(new FirefoxOptions());
-	        } else {
-	            throw new RuntimeException("Invalid browser: " + browser);
-	        }
+    @BeforeMethod
+    @Parameters("browser")
+    public void setup(String browser) {
 
-	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	        driver.get("https://demoblaze.com/");
-	    }
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--start-maximized");
+            driver = new ChromeDriver(options);
+        }
 
-	    private void login(String username, String password) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().setSize(new Dimension(1920,1080));
+        driver.get("https://demoblaze.com/");
+    }
 
-	        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    private void login(String username, String password) {
 
-	        WebElement loginBtn = wait.until(
-	                ExpectedConditions.elementToBeClickable(By.id("login2"))
-	        );
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-	        loginBtn.click();
+        WebElement loginBtn = wait.until(
+                ExpectedConditions.presenceOfElementLocated(By.id("login2"))
+        );
 
-	        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("loginusername"))).sendKeys(username);
-	        driver.findElement(By.id("loginpassword")).sendKeys(password);
+        // scroll
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", loginBtn);
 
-	        driver.findElement(By.xpath("//button[text()='Log in']")).click();
-	    }
+        // try normal click, if fails use JS
+        try {
+            loginBtn.click();
+        } catch (Exception e) {
+            ((JavascriptExecutor) driver).executeScript("arguments[0].click();", loginBtn);
+        }
 
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("loginusername"))).sendKeys(username);
+        driver.findElement(By.id("loginpassword")).sendKeys(password);
+        driver.findElement(By.xpath("//button[text()='Log in']")).click();
+    }
 
-	    
-	    @Test(dataProvider = "validData")
-	    public void validLoginTest(String username, String password) {
+    @Test(dataProvider = "validData")
+    public void validLoginTest(String username, String password) {
 
-	        login(username, password);
+        login(username, password);
 
-	        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-	        String user = wait.until(
-	                ExpectedConditions.visibilityOfElementLocated(By.id("nameofuser"))
-	        ).getText();
+        String user = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("nameofuser"))
+        ).getText();
 
-	        Assert.assertTrue(user.contains("Welcome"));
-	        System.out.println("Valid login passed");
-	    }
-	    
-	    @Test(dataProvider = "invalidData")
-	    public void invalidLoginTest(String username, String password, String expectedMessage) {
+        Assert.assertTrue(user.contains("Welcome"));
+        System.out.println("Valid login passed");
+    }
 
-	        login(username, password);
+    @Test(dataProvider = "invalidData")
+    public void invalidLoginTest(String username, String password, String expectedMessage) {
 
-	        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        login(username, password);
 
-	        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
-	        String actualMsg = alert.getText();
-	        alert.accept();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-	        Assert.assertEquals(actualMsg, expectedMessage);
-	        System.out.println("Invalid test passed: " + actualMsg);
-	    }
+        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+        String actualMsg = alert.getText();
+        alert.accept();
 
-	    @AfterMethod
-	    public void tearDown() {
-	        if (driver != null) {
-	            driver.quit();
-	        }
-	    }
+        Assert.assertEquals(actualMsg, expectedMessage);
+        System.out.println("Invalid test passed: " + actualMsg);
+    }
 
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
 }
